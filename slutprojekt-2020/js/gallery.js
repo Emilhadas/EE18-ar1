2b3a1ebbded84af3ee7244f2ca3737dc121fc076
emilhadas <emil.hadas@elev.ga.ntig.se>
2020-05-20 12:49:06 +0200

    test

---
+++ slutprojekt-2020/js/gallery.js
@@ -0,0 +1,6 @@
+// Selecting images
+const images = document.querySelector('.gallery');
+
+// Selecting buttons
+const nextButton = document.querySelector('#nextButton');
+const prevButton = document.querySelector('#prevButton');
